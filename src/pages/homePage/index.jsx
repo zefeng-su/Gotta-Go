8a3f7bfbf2fb55005f06b4413110c1fd20885590
Zefeng Su <imnot.zefeng@gmail.com>
2023-07-05 17:38:46 +0800

    added navbar

---
+++ src/pages/homePage/index.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import Navbar from "pages/navbar";
+
+function HomePage () {
+    return (
+        <Box> 
+            <Navbar />
+        </Box>       
+    )
+} 
+
+export default HomePage;
