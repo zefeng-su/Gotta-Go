b0d12cac9fe9194d6b87d0f6e64e92683fb9bfa0
Zefeng Su <imnot.zefeng@gmail.com>
2023-07-14 16:50:37 +0800

    profileEdit page done

--- src/pages/profilePage/ProfileEdit.jsx
+++ src/pages/profilePage/ProfileEdit.jsx
@@ -1,99 +1,148 @@
 import { Box, Typography, useTheme, useMediaQuery, Grid, TextField, Button } from "@mui/material";
 import { Formik } from "formik";
-import { useNavigate } from "react-router-dom";
-import Dropzone from "react-dropzone";
+import { useNavigate, useParams } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 function ProfileEdit() {
   const theme = useTheme();
   const { palette } = useTheme();
   const isNonMobileScreens = useMediaQuery("(min-width: 1000px)");
   const navigate = useNavigate();
+  const { userId } = useParams(); // Retrieve the userId from the URL
+
+  const token = useSelector((state) => state.token); // Retrieve the token from your Redux store
+
+  const handleSave = async (values) => {
+    if (window.confirm("Confirm the change? You have to login again after clicking OK")) {
+      try {
+        const userProfile = { ...values }; // Create a copy of the updated values object
+        const existingProfile = await fetch(`http://localhost:3001/users/${userId}`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }).then((response) => response.json());
+  
+        // Merge the existing picturePath with the updated values
+        userProfile.picturePath = existingProfile.picturePath;
+  
+        await fetch(`http://localhost:3001/users/${userId}`, {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(userProfile),
+        });
+        navigate(`/`); // Navigate to the home page after saving the profile
+      } catch (error) {
+        console.error(error);
+        // Handle error
+      }
+    }
+  };
+
+  const handleDeleteAccount = async () => {
+    if (window.confirm("Are you sure you want to delete your account?")) {
+      try {
+        await fetch(`http://localhost:3001/users/${userId}/delete`, {
+          method: "DELETE",
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+        navigate(`/`); // Navigate to the home page after deleting the account
+      } catch (error) {
+        console.error(error);
+        // Handle error
+      }
+    }
+  };
 
   return (
-    <Formik>
-      <Box
-        width={isNonMobileScreens ? "50%" : "93%"}
-        padding="2rem"
-        margin="2rem auto"
-        borderRadius="0.75rem"
-        backgroundColor={theme.palette.background.alt}
-      >
-        <Typography fontWeight="700" variant="h4" sx={{ mb: "1rem" }} textAlign="center">
-          Edit your profile
-        </Typography>
+    <Formik
+      initialValues={{ firstName: "", lastName: "",   }}
+      onSubmit={(values) => handleSave(values)}
+    >
+      {({ values, handleChange, handleSubmit }) => (
+        <Box
+          width={isNonMobileScreens ? "50%" : "93%"}
+          padding="2rem"
+          margin="2rem auto"
+          borderRadius="0.75rem"
+          backgroundColor={theme.palette.background.alt}
+        >
+          <Typography fontWeight="700" variant="h4" sx={{ mb: "1rem" }} textAlign="center">
+            Edit your profile
+          </Typography>
 
-        <Grid container spacing={2}>
-          <Grid item xs={6}>
-            <TextField label="First Name" fullWidth />
+          <Grid container spacing={2}>
+            <Grid item xs={6}>
+              <TextField
+                label="First Name"
+                fullWidth
+                name="firstName"
+                value={values.firstName}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                label="Last Name"
+                fullWidth
+                name="lastName"
+                value={values.lastName}
+                onChange={handleChange}
+              />
+            </Grid>
           </Grid>
-          <Grid item xs={6}>
-            <TextField label="Last Name" fullWidth />
-          </Grid>
-        </Grid>
 
-        <Dropzone
-         acceptedFiles=".jpg,.jpeg,.png"
-         multiple={false}>
-          {({ getRootProps, getInputProps }) => (
-            <Box
-              {...getRootProps()}
+          <Grid container justifyContent="center">
+            <Button
+              type="submit"
+              onClick={handleSubmit}
               sx={{
-                border: "2px dashed #999",
-                borderRadius: "0.75rem",
-                padding: "2rem",
-                marginTop: "2rem",
-                textAlign: "center",
+                m: "2rem",
+                p: "1rem",
+                backgroundColor: palette.primary.main,
+                color: palette.background.alt,
+                "&:hover": { color: palette.primary.main },
               }}
             >
-              <input {...getInputProps()} />
-              <Typography variant="body1">Drag and drop files here</Typography>
-              <Typography variant="body2">or click to browse</Typography>
-            </Box>
-          )}
-        </Dropzone>
+              Save
+            </Button>
 
-        <Grid container justifyContent="center">
-          <Button
-            sx={{
-              m: "2rem",
-              p: "1rem",
-              backgroundColor: palette.primary.main,
-              color: palette.background.alt,
-              "&:hover": { color: palette.primary.main },
-            }}
-          >
-            Save
-          </Button>
+            <Button
+              onClick={() => navigate(`/home`)}
+              sx={{
+                m: "2rem",
+                p: "1rem",
+                backgroundColor: palette.primary.main,
+                color: palette.background.alt,
+                "&:hover": { color: palette.primary.main },
+              }}
+            >
+              Cancel
+            </Button>
+          </Grid>
 
-          <Button
-            onClick={() => navigate(`/home`)}
-            sx={{
-              m: "2rem",
-              p: "1rem",
-              backgroundColor: palette.primary.main,
-              color: palette.background.alt,
-              "&:hover": { color: palette.primary.main },
-            }}
-          >
-            Cancel
-          </Button>
-        </Grid>
-
-        <Grid container justifyContent="center">
-          <Button
-            sx={{
-              m: "2rem",
-              mt:"0rem",
-              p: "1rem",
-              backgroundColor: "red",
-              color: palette.background.alt,
-              "&:hover": { color: palette.primary.main },
-            }}
-          >
-            !DELETE ACCOUNT!
-          </Button>
-        </Grid>
-      </Box>
+          <Grid container justifyContent="center">
+            <Button
+              onClick={handleDeleteAccount}
+              sx={{
+                m: "2rem",
+                mt: "0rem",
+                p: "1rem",
+                backgroundColor: "red",
+                color: palette.background.alt,
+                "&:hover": { color: palette.primary.main },
+              }}
+            >
+              !DELETE ACCOUNT!
+            </Button>
+          </Grid>
+        </Box>
+      )}
     </Formik>
   );
 }
